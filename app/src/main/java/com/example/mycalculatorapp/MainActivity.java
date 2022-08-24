684b8c7c944179551c0e618b0a531af74ebc1c61
JensLundsgaard <111328305+JensLundsgaard@users.noreply.github.com>
2022-08-24 12:54:02 -0500

    Refactoring and fixing problems for basic operations.

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -21,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.output);
         String inputString = editText1.getText().toString();
         String accuracyString = editText2.getText().toString();
+        if(inputString.equals("")){
+            inputString = "0.0";
+        }
+        if(accuracyString.equals("")){
+            accuracyString = "20";
+        }
         double input = Double.parseDouble(inputString);
         int accuracy = Integer.parseInt(accuracyString);
         double output = TaylorMath.sin(input, accuracy);
@@ -36,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.output);
         String inputString = editText1.getText().toString();
         String accuracyString = editText2.getText().toString();
+        if(inputString.equals("")){
+            inputString = "0.0";
+        }
+        if(accuracyString.equals("")){
+            accuracyString = "20";
+        }
+
         double input = Double.parseDouble(inputString);
         int accuracy = Integer.parseInt(accuracyString);
         double output = TaylorMath.cos(input, accuracy);
@@ -51,6 +64,13 @@ public class MainActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.output);
         String inputString = editText1.getText().toString();
         String accuracyString = editText2.getText().toString();
+        if(inputString.equals("")){
+            inputString = "0.0";
+        }
+        if(accuracyString.equals("")){
+            accuracyString = "20";
+        }
+
         double input = Double.parseDouble(inputString);
         int accuracy = Integer.parseInt(accuracyString);
         double output = TaylorMath.ln(input, accuracy);
@@ -67,6 +87,13 @@ public class MainActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.output);
         String inputString = editText1.getText().toString();
         String accuracyString = editText2.getText().toString();
+        if(inputString.equals("")){
+            inputString = "0.0";
+        }
+        if(accuracyString.equals("")){
+            accuracyString = "20";
+        }
+
         double input = Double.parseDouble(inputString);
         int accuracy = Integer.parseInt(accuracyString);
         double output = TaylorMath.exp(input, accuracy);
@@ -78,13 +105,20 @@ public class MainActivity extends AppCompatActivity {
     }
     public void outputAdd(View view){
         EditText editText1 = (EditText) findViewById(R.id.operand1);
-        EditText editText2 = (EditText) findViewById(R.id.input2);
+        EditText editText2 = (EditText) findViewById(R.id.operand2);
         TextView text = (TextView) findViewById(R.id.output);
         String operand1String = editText1.getText().toString();
         String operand2String = editText2.getText().toString();
+        if(operand1String.equals("")){
+            operand1String = "0.0";
+        }
+        if(operand2String.equals("")){
+            operand2String = "0.0";
+        }
+
         double op1 = Double.parseDouble(operand1String);
         double op2 = Double.parseDouble(operand2String);
-        double output = op1 + op2;
+        double output = op1+op2;
         String outputString = String.valueOf(output);
         text.setText(outputString);
     }
@@ -94,6 +128,13 @@ public class MainActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.output);
         String operand1String = editText1.getText().toString();
         String operand2String = editText2.getText().toString();
+        if(operand1String.equals("")){
+            operand1String = "0.0";
+        }
+        if(operand2String.equals("")){
+            operand2String = "0.0";
+        }
+
         double op1 = Double.parseDouble(operand1String);
         double op2 = Double.parseDouble(operand2String);
         double output = op1 - op2;
@@ -106,6 +147,13 @@ public class MainActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.output);
         String operand1String = editText1.getText().toString();
         String operand2String = editText2.getText().toString();
+        if(operand1String.equals("")){
+            operand1String = "0.0";
+        }
+        if(operand2String.equals("")){
+            operand2String = "0.0";
+        }
+
         double op1 = Double.parseDouble(operand1String);
         double op2 = Double.parseDouble(operand2String);
         double output = op1 * op2;
@@ -118,10 +166,55 @@ public class MainActivity extends AppCompatActivity {
         TextView text = (TextView) findViewById(R.id.output);
         String operand1String = editText1.getText().toString();
         String operand2String = editText2.getText().toString();
+        if(operand1String.equals("")){
+            operand1String = "0.0";
+        }
+        if(operand2String.equals("")){
+            operand2String = "1.0";
+        }
         double op1 = Double.parseDouble(operand1String);
         double op2 = Double.parseDouble(operand2String);
         double output = op1 / op2;
         String outputString = String.valueOf(output);
         text.setText(outputString);
+    }
+
+    public void showOutputTaylor(View view){
+        EditText editText1 = (EditText) findViewById(R.id.input1);
+        EditText editText2 = (EditText) findViewById(R.id.input2);
+        TextView text = (TextView) findViewById(R.id.output);
+        String inputString = editText1.getText().toString();
+        String accuracyString = editText2.getText().toString();
+        if(inputString.equals("")){
+            inputString = "0.0";
+        }
+        if(accuracyString.equals("")){
+            accuracyString = "20";
+        }
+        double input = Double.parseDouble(inputString);
+        int accuracy = Integer.parseInt(accuracyString);
+        double output = 0.0;
+        switch (view.getId()) {
+            case R.id.button:
+                output = TaylorMath.sin(input, accuracy);
+                break;
+            case R.id.button2:
+                output = TaylorMath.cos(input, accuracy);
+                break;
+            case R.id.button3:
+                output = TaylorMath.ln(input, accuracy);
+                break;
+            case R.id.button4:
+                output = TaylorMath.exp(input, accuracy);
+                break;
+        }
+        // Source: https://stackoverflow.com/questions/153724/how-to-round-a-number-to-n-decimal-places-in-java
+        // This code allows you to round a double down to a certain number of digits.
+        output = (double)Math.round(output * 100000d) / 100000d;
+        String outputString = String.valueOf(output);
+        text.setText(outputString);
+    }
+    public void showOutputBasic(View view){
+
     }
 }
